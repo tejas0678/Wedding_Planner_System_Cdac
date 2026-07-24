066e343ec490a6ff9d3eb118c93d5946c3dd6b9e
Pratiksha <pdchikane21@gmail.com>
2026-07-24 10:53:16 +0530

    Added updated bookings, planner browsing and search functionality

--- Frontend/wedding-planner-frontend/src/routes/ClientRoutes.jsx
+++ Frontend/wedding-planner-frontend/src/routes/ClientRoutes.jsx
@@ -2,14 +2,19 @@ import { Route } from "react-router-dom";
 import { AppDataProvider } from "../context/client/AppDataContext"; 
 import ClientLayout from "../layouts/ClientLayout";
 import Dashboard from "../pages/client/Dashboard";
+import Planners from "../pages/client/Planners";
+import Bookings from "../pages/client/Bookings";
 
 const ClientRoutes = (
   <>
-  <Route path="/" element={<AppDataProvider>
+  <Route path="/client" element={<AppDataProvider>
           <ClientLayout />
         </AppDataProvider>}>
     <Route index element={<Dashboard />} />
     <Route path="dashboard" element={<Dashboard />} />
+    {/* Planner routes */}
+    <Route path="planners" element={<Planners />} />
+    <Route path="bookings" element={<Bookings />} />
   </Route>
   </>
 );
