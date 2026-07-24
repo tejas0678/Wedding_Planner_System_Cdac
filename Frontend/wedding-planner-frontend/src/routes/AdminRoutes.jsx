415b1ebc37cc667b3792c32b56da3b297bee4836
Soniya Ajmire <ajmire30soniya@gmail.com>
2026-07-24 15:28:18 +0530

    added ManageBookings Route

--- Frontend/wedding-planner-frontend/src/routes/AdminRoutes.jsx
+++ Frontend/wedding-planner-frontend/src/routes/AdminRoutes.jsx
@@ -1,9 +1,37 @@
 import { Route } from "react-router-dom";
 
+import Layout from "../layouts/admin/Layout";
+import ManageClients from "../pages/admin/ManageClients";
+import Dashboard from "../pages/admin/Dashboard";
+import ManagePlanners from "../pages/admin/ManagePlanners";
+import ManageBookings from "../pages/admin/ManageBookings";
+
 
 const AdminRoutes = (
   <>
-  
+    {/* Admin Layout */}
+    <Route path="/admin" element={<Layout />}>
+
+      {/* Dashboard */}
+       <Route
+        path="dashboard"
+        element={<Dashboard />}
+      />
+
+      <Route
+        path="clients"
+        element={<ManageClients />}
+      />
+       <Route
+        path="planners"
+        element={<ManagePlanners />}
+      />
+       <Route
+        path="bookings"
+        element={<ManageBookings />}
+      />
+     
+    </Route>
   </>
 );
 
