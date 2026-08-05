2133d0206819e7f537f15e0457278122a16d478c
Tejas Sayane <tejassayane067@gmail.com>
2026-08-05 07:09:48 +0530

    chat bot config

--- Frontend/wedding-planner-frontend/src/App.jsx
+++ Frontend/wedding-planner-frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import IndexRoutes from "./routes/IndexRoutes";
+import ChatWidget from "./components/common/ChatWidget";
 
 function App() {
-  return <IndexRoutes />;
+  return (
+    <>
+      <IndexRoutes />
+      <ChatWidget />
+    </>
+  );
 }
 
 export default App;
